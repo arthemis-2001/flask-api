2d4c48543bbc5ec065053d9a91b64070be0461d6
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-05-30 20:12:22 +0200

    Added create_user route

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ now = datetime.now()
 
 # models
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
@@ -35,10 +35,69 @@ class User(db.Model):
     posts = db.relationship("BlogPost")
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 
-if __name__ == '__main__':
+# routes
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 200
+
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_descending():
+    pass
+
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_users_ascending():
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_user(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_blog_post(blog_post_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_blog_post(blog_post_id):
+    pass
+
+
+if __name__ == "__main__":
     app.run(debug=True)
